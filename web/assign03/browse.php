473f6dfa4272cec24749c9a00dbf1e4a072f02e4
JTSchlottmann <schlottmann.jeremy@gmail.com>
2018-10-09 23:19:25 -0600

    next test

--- web/assign03/browse.php
+++ web/assign03/browse.php
@@ -10,7 +10,7 @@ session_start();
       <meta charset="utf-8">
       <meta name="viewport" content="width=device-width, initial-scale=1">
       <link rel="stylesheet" href="shop.css">
-      <script src="http://code.jquery.com/jquery-3.3.1.min.js" integrity="sha256-2Kok7MbOyxpgUVvAk/HJ2jigOSYS2auK4Pfzbm7uH60=" crossorigin="anonymous"></script>
+      <script src="http://code.jquery.com/jquery-3.3.1.js" integrity="sha256-2Kok7MbOyxpgUVvAk/HJ2jigOSYS2auK4Pfzbm7uH60=" crossorigin="anonymous"></script>
       <script>
          function addToCart(book) {
 	 $.ajax({type: "POST", url: "./add.php", data: { book: book}})

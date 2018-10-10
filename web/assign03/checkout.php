112fecea6f0c0d6fa4aeffe746f528d5e7a30e87
JTSchlottmann <schlottmann.jeremy@gmail.com>
2018-10-10 14:02:10 -0600

    added checkout and confirm pages

---
+++ web/assign03/checkout.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+
+<?php session_start(); ?>
+
+<html lang="en">
+   <head>
+      <title>Checkout</title>
+      <meta charset="utf-8">
+      <meta name="viewport" content="width=device-width, initial-scale=1">  
+      <link rel="stylesheet" href="shop.css">
+   </head>
+   <body>
+      <form>
+      
+      </form>
+   </body>
+</html>

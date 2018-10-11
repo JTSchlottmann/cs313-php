f8b118d450c49adf9884cb6d60442c1d1f5a1c53
JTSchlottmann <JTSchlottmann@users.noreply.github.com>
2018-10-10 23:55:31 -0600

    Hopefully completed

--- web/assign03/checkout.php
+++ web/assign03/checkout.php
@@ -10,8 +10,27 @@
       <link rel="stylesheet" href="shop.css">
    </head>
    <body>
-      <form>
-      
+      <form method="POST" action="confirm.php">
+         <h1>Checkout</h1>
+         <hr></br>
+           
+	 Street Address: <input type="text" class="street" name="street" placeholder="Street Address">
+         </br></br>
+
+	 City: <input type="text" class="city" name="city" placeholder="City">
+
+         </br></br>
+
+	 State: <input type="text" class="state" name="state" placeholder="State">
+
+         </br></br>
+
+	 ZIP Code: <input type="text" class="zip" name="zip" placeholder="Zip Code">
+         
+	 </br></br>
+
+	 <a href="./cart.php" class="button">Return to Cart</a>
+         <input type="submit" value="Confirm" id="submit" class="button">
       </form>
    </body>
 </html>

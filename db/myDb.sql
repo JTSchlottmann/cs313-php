dcf30192c4f4259f5cb562c30760a9520fc9802b
JTSchlottmann <schlottmann.jeremy@gmail.com>
2018-11-11 16:53:06 -0700

    added db directory

---
+++ db/myDb.sql
@@ -0,0 +1,22 @@
+CREATE TABLE public.book (
+	id	SERIAL NOT NULL PRIMARY KEY,
+	title   VARCHAR(50),
+	author  VARCHAR(50),
+	prices  FLOAT
+);
+
+CREATE TABLE public.user (
+	id	SERIAL NOT NULL PRIMARY KEY,
+        fname   VARCHAR(50)  NOT NULL,
+        lname   VARCHAR(50)  NOT NULL,
+        address VARCHAR(250) NOT NULL,
+        city    VARCHAR(50)  NOT NULL,
+        state   VARCHAR(50)  NOT NULL,
+        zip     VARCHAR(50)  NOT NULL
+);
+
+CREATE TABLE public.history (
+        date    DATE NOT NULL,
+	user_id INT NOT NULL REFERENCES public.user(id),
+	book_id INT NOT NULL REFERENCES public.book(id)
+);
